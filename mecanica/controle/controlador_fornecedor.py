6da1cc3c181f0b3840443211dab4ae9dac19f06d
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-18 18:09:37 -0300

    Update controlador_fornecedor.py

--- mecanica/controle/controlador_fornecedor.py
+++ mecanica/controle/controlador_fornecedor.py
@@ -1,3 +1,6 @@
+from entidade.fornecedor import Fornecedor
+
+
 class ControladorFornecedor:
     def __init__(self):
         self.__fonecedores = []
@@ -6,4 +9,11 @@ class ControladorFornecedor:
     def fornecedores(self):
         return self.__fonecedores
 
+    def inclui_fornecedor(self, nome, telefone, email, cnpj):
+        novo_fornecedor = Fornecedor(nome, telefone, email, cnpj)
+        for fornecedor in self.__fonecedores:
+            if fornecedor.cnpj == novo_fornecedor.cnpj:
+                return None
+        else:
+            self.__fonecedores.append(novo_fornecedor)
 
