59fea90e67eb12f5221dfbe0b44742e7d8626511
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-18 18:05:49 -0300

    Create tela_sistemas.py

---
+++ mecanica/limite/tela_sistemas.py
@@ -0,0 +1,15 @@
+
+
+class TelaSistema:
+    def tela_opcoes(self):
+        print("-------- SisLivros ---------")
+        print("Escolha sua opcao")
+        print("1 - Fornecedores")
+        print("2 - Oleos")
+        print("3 - Modelos")
+        print("4 - Clientes")
+        print("5 - Modelos")
+        print("6 - Troca de Óleo")
+        print("0 - Finalizar sistema")
+        opcao = int(input("Escolha a opcao:"))
+        return opcao
