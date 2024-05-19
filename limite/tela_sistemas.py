747a17c138ce0db14775bf5f0b772f1e91504943
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-19 18:57:30 -0300

    Update tela_sistemas.py

--- limite/tela_sistemas.py
+++ limite/tela_sistemas.py
@@ -4,12 +4,9 @@ class TelaSistema:
     def tela_opcoes(self):
         print("-------- SisLivros ---------")
         print("Escolha sua opcao")
-        print("1 - Fornecedores")
-        print("2 - Oleos")
-        print("3 - Modelos")
-        print("4 - Clientes")
-        print("5 - Modelos")
-        print("6 - Troca de Óleo")
+        print("1 - Livros")
+        print("2 - Amigos")
+        print("3 - Emprestimos")
         print("0 - Finalizar sistema")
         opcao = int(input("Escolha a opcao:"))
         return opcao
