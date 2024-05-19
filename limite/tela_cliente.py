276c7588bb348d09ddbc509161a2fe8efeb1f4e6
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-19 19:59:55 -0300

    Update tela_cliente.py

--- limite/tela_cliente.py
+++ limite/tela_cliente.py
@@ -7,6 +7,8 @@ class TelaCliente:
         print("2 - Alterar Cliente")
         print("3 - Listar Cliente")
         print("4 - Excluir Cliente")
+        print("-------- VEÍCULOS --------")
+        print("Escolha a opcao")
         print("5 - Incluir Veiculo")
         print("6 - Alterar Veículo")
         print("7 - Excluir Veiculo")
@@ -21,7 +23,7 @@ class TelaCliente:
         return cpf
 
     def pega_dados_moto(self):
-        print("-------- DADOS CLIENTE ----------")
+        print("-------- DADOS MOTO ----------")
         modelo_moto = input("Modelo ")
         placa_moto = input("Placa: ")
         km_moto = input("Kilometragem: ")
@@ -31,10 +33,10 @@ class TelaCliente:
     def pega_dados_cliente(self):
         print("-------- DADOS CLIENTE ----------")
         nome_cliente = input("Nome: ")
-        cpf_cliente = input("CPF: ")
         telefone_cliente = input("Telefone: ")
         email_cliente = input("Email: ")
-        return {"nome": nome_cliente, "cpf": cpf_cliente, "telefone": telefone_cliente, "email": email_cliente}
+        cpf_cliente = input("CPF: ")
+        return {"nome": nome_cliente, "telefone": telefone_cliente, "email": email_cliente, "cpf": cpf_cliente}
     def mostra_cliente(self, dados_cliente):
         print("NOME DO CLIENTE: ", dados_cliente["nome"])
         print("CPF DO CLIENTE: ", dados_cliente["cpf"])
@@ -42,9 +44,11 @@ class TelaCliente:
         print("EMAIL DO CLIENTE", dados_cliente["email"])
         print("\n")
 
-    def seleciona_cliente(self):
-        cpf = input("Cpf do cliente que deseja selecionar: ")
-        return cpf
+    def mostra_veiculo(self, dados_veiculo):
+        print("MODELO DO VEICULO: ", dados_veiculo["modelo"])
+        print("KILOMETRAGEM: ", dados_veiculo["kilometragem"])
+        print("PLACA: ", dados_veiculo["placa"])
+        print("\n")
 
     def seleciona_moto(self):
         placa_moto = input("Placa da moto que deseja selecionar: ")
