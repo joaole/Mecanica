f4984ad64153a33ea098cef676902620c6434459
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-20 16:41:49 -0300

    Update cliente.py

--- Mecanica/entidade/cliente.py
+++ Mecanica/entidade/cliente.py
@@ -30,18 +30,4 @@ class Cliente(Pessoa):
         for veiculo in self.__veiculos:
             if veiculo.placa_moto == placa_moto:
                 return veiculo
-
-    def validar_cpf(self, cpf: str) -> bool:
-        cpf = ''.join(filter(str.isdigit, cpf))
-
-        if len(cpf) != 11 or cpf == cpf[0] * 11:
-            return False
-
-        for i in range(9, 11):
-            soma = sum(int(cpf[j]) * (i + 1 - j) for j in range(i))
-            digito = (soma * 10) % 11
-            if digito == 10:
-                digito = 0
-            if cpf[i] != str(digito):
-                return False
-        return True
+                
