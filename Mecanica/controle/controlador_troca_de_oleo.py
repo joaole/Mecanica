31fb2d3bc54ed38d9153ee93d5ddad044882bef9
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-20 17:56:34 -0300

    Update controlador_troca_de_oleo.py

--- Mecanica/controle/controlador_troca_de_oleo.py
+++ Mecanica/controle/controlador_troca_de_oleo.py
@@ -1,102 +1,45 @@
 from entidade.troca_de_oleo import TrocaDeOleo
-from entidade.veiculo import Veiculo
-from entidade.cliente import Cliente
-from entidade.oleo import Oleo
 from limite.tela_troca_de_oleo import TelaTrocaDeOleo
 
-class ControladorTrocaDeOleo:
-    def __init__(self):
-        self.__trocas = []
-        self.__quantidade_de_oleo_total = 0
-        self.__tela_troca_de_oleo = TelaTrocaDeOleo
 
-    def pega_dados_troca(self, codigo):
+class ControladorTrocaDeOleo:
+    def __init__(self,controlador_sistema, modelo=None):
+        self.__controlador_sistema = controlador_sistema
+        self.__tela_troca_de_oleo = TelaTrocaDeOleo()
+        self.__trocas = []
+
+    def gerar_codigo(self):
+        codigo = len(self.__trocas) + 1
+        return codigo
+
+    def pega_troca_por_codigo(self, codigo):
         for troca in self.__trocas:
             if troca.codigo == codigo:
                 return troca
-        return None
+        else:
+            return None
+    def efetuar_troca(self, dados_troca):
+        cliente = self.__controlador_sistema.controlador_cliente.pega_cliente_por_cpf(dados_troca["cpf_cliente"])
+        if cliente is not None:
+            veiculo = self.__controlador_sistema.controlador_cliente.pega_moto_por_placa(cliente, dados_troca["placa_moto"])
+            if veiculo is not None:
+                codigo = self.gerar_codigo()
+                nova_troca = TrocaDeOleo(veiculo, cliente, dados_troca["data_entrada"], None, codigo)
+                for troca in self.__trocas:
+                    if troca.cliente != nova_troca.cliente and troca.veiculo != nova_troca.veiculo and troca.data_entrada != nova_troca.data_entrada:
+                        self.__trocas.append(nova_troca)
+                    else:
+                        self.__tela_troca_de_oleo.mostra_mensagem("ATENCAO: Ja possui uma troca com o mesmo cliente, veiculo e data de entrada")
 
-    def fazer_troca_de_oleo(self, veiculo: Veiculo, cliente: Cliente, oleo: Oleo, data_entrada: str, data_saida: str, quantidade_oleo: float):
-        codigo = len(self.__trocas) + 1
-        nova_troca = TrocaDeOleo(veiculo, cliente, oleo, data_saida, data_entrada, codigo)
-        nova_troca.quantidade_oleo = quantidade_oleo
-        self.calcular_valor_final(nova_troca)
-        self.__trocas.append(nova_troca)
-        self.__quantidade_de_oleo_total += quantidade_oleo
-        print('Troca Realizada!')
 
-    def incluir_troca(self, troca: TrocaDeOleo):
-        self.__trocas.append(troca)
-        self.__quantidade_de_oleo_total += troca.quantidade_oleo
 
     def cancelar_troca(self, codigo):
-        troca_para_cancelar = self.pega_dados_troca(codigo)
-        if troca_para_cancelar:
-            self.__trocas.remove(troca_para_cancelar)
-            self.__quantidade_de_oleo_total -= troca_para_cancelar.quantidade_oleo
+        for troca in self.__trocas:
+            if troca.codigo == codigo:
+                self.__trocas.remove(troca)
 
-    def ver_detalhes_troca(self):
-        print("\nDetalhes da troca de óleo")
-        codigo = int(input("Código da troca: "))
-        troca = self.pega_dados_troca(codigo)
-        if troca:
-            print(f"Código: {troca.codigo}")
-            print(f"Veículo: {troca.veiculo.placa_moto}")
-            print(f"Cliente: {troca.cliente.nome}")
-            print(f"Óleo: {troca.oleo.marca} - {troca.oleo.expessura}")
-            print(f"Data de entrada: {troca.data_entrada}")
-            print(f"Data de saída: {troca.data_saida}")
-            print(f"Quantidade de óleo: {troca.quantidade_oleo} litros")
-            print(f"Valor final: R$ {troca.valor_final}")
-            print(f"Tempo na oficina: {self.calcular_tempo_na_oficina(codigo)} dias")
+    def calcular_quantidade_oleo(self):
+
+
         else:
-            print("Troca de óleo não encontrada!")
-
-    def ver_quantidade_total_oleo(self):
-        quantidade_total = self.calcular_quantidade_total_de_oleo()
-        print(f"\nQuantidade total de óleo utilizado: {quantidade_total} litros")
-
-    def calcular_valor_final(self, troca: TrocaDeOleo):
-        troca.valor_final = troca.oleo.valor * troca.quantidade_oleo
-
-    def calcular_quantidade_total_de_oleo(self):
-        return self.__quantidade_de_oleo_total
-
-    def calcular_tempo_na_oficina(self, codigo):
-        troca = self.pega_dados_troca(codigo)
-        if troca:
-            data_entrada = troca.data_entrada
-            data_saida = troca.data_saida
-            return (data_saida - data_entrada)
-        return None
-
-    def obter_veiculo(self):
-        placa_moto = input("Placa do veículo: ")
-        km_moto = float(input("Quilometragem do veículo: "))
-        modelo = input("Modelo do veículo: ")  # Aqui deve ser um objeto Modelo, simplificando para string
-        return Veiculo(placa_moto, km_moto, modelo)
-
-    def obter_cliente(self):
-        nome = input("Nome do cliente: ")
-        telefone = input("Telefone do cliente: ")
-        email = input("Email do cliente: ")
-        cpf = int(input("CPF do cliente: "))
-        return Cliente(nome, telefone, email, cpf)
-
-    def obter_oleo(self):
-        marca = input("Marca do óleo: ")
-        expessura = input("Expessura do óleo: ")
-        valor = float(input("Valor do óleo por litro: "))
-        codigo = int(input("Código do óleo: "))
-        fornecedor = input("Fornecedor do óleo: ")  # Aqui deve ser um objeto Fornecedor, simplificando para string
-        return Oleo(fornecedor, marca, expessura, valor, codigo)
-
-
-
-    def abre_tela(self):
-        lista_opcoes = {1: self.fazer_troca_de_oleo, 2: self.cancelar_troca, 3: self.ver_detalhes_troca,
-                        4: self.ver_quantidade_total_oleo}
-
-        continua = True
-        while continua:
-            lista_opcoes[self.__tela_troca_de_oleo.tela_opcoes(self)]()
+            return 0
