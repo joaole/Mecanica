0870be1326e8ca9ba4e8adb69bd8519acd4ddfc8
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-20 12:18:57 -0300

    Update modelo.py

--- Mecanica/entidade/modelo.py
+++ Mecanica/entidade/modelo.py
@@ -1,8 +1,9 @@
 
 
 class Modelo:
-    def __init__(self, nome, quantidade_oleo, codigo):
+    def __init__(self, nome, expessura, quantidade_oleo, codigo):
         self.__nome = nome
+        self.__expessura = expessura
         self.__quantidade_oleo = quantidade_oleo
         self.__oleos = []
         self.__codigo = codigo
@@ -14,6 +15,14 @@ class Modelo:
     @nome.setter
     def nome(self, nome):
         self.__nome = nome
+
+    @property
+    def expessura(self):
+        return self.__expessura
+
+    @expessura.setter
+    def expessura(self, expessura):
+        self.__expessura = expessura
 
     @property
     def quantidade_oleo(self):
@@ -34,3 +43,17 @@ class Modelo:
     @property
     def oleos(self):
         return self.__oleos
+
+    def incluir_oleo(self, oleo):
+        for ol in self.__oleos:
+            if ol.codigo == oleo.codigo:
+                return None
+        else:
+            self.__oleos.append(oleo)
+
+    def exclui_oleo(self, oleo):
+        for ol in self.__oleos:
+            if ol.codigo == oleo.codigo:
+                self.__oleos.remove(oleo)
+        else:
+            return None
