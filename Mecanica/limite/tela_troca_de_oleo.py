782940a6b4e00cc5d43aaa7b84fca2f586cefd15
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-05-20 22:47:50 -0300

    Update tela_troca_de_oleo.py

--- Mecanica/limite/tela_troca_de_oleo.py
+++ Mecanica/limite/tela_troca_de_oleo.py
@@ -5,30 +5,46 @@ class TelaTrocaDeOleo:
     def tela_opcoes(self):
         print("-------- TROCA DE OLEO ----------")
         print("Escolha a opcao")
-        print("1 - Realizar Troca")
-        print("2 - Alterar Troca")
-        print("3 - Listar Cliente")
-        print("4 - Cancelar Troca")
-        print("5 - Finalizar Troca")
+        print("1 - Cadastrar Troca")
+        print("2 - Finalizar Troca")
+        print("3 - Listar Trocas")
+        print("4 - Listrar Trocas Efetuadas")
         print("0 - Retornar")
 
         opcao = int(input("Escolha a opcao: "))
         return opcao
 
-    def seleciona_cliente(self):
-        cpf = input("CPF do cliente que deseja selecionar: ")
-        return cpf
+    def mostra_troca(self, dados_troca):
+        print("NOME DO CLIENTE: ", dados_troca["nome"])
+        print("PLACA DA MOTO: ", dados_troca["placa"])
+        print("MODELO DA MOTO: ", dados_troca["modelo"])
+        print("DATA DE ENTRADA DO VEICULO: ", dados_troca["data_entrada"])
+        print("ESPESSURA DO OLEO", dados_troca["espessura"])
+        if dados_troca["oleo"] is not None:
+            print("MODELO DO OLEO ESCOLHIDO: ", dados_troca["oleo"])
+        if dados_troca["valor_final"] is not None:
+            print("VALOR DA TROCA EFETUADA: R$", dados_troca["valor_final"])
+        if dados_troca["data_saida"] is not None:
+            print("DATA DE SAIDA DO VEICULO: ", dados_troca["data_saida"])
+        print("CODIGO DA TROCA: ", dados_troca["codigo"])
+        print("\n")
 
-    def pega_codigo_modelo(self):
-        codigo_modelo = input("CODIGO DE MODELO DO VEICULO: ")
-        return codigo_modelo
+
+    def seleciona_troca(self):
+        codigo = input("Codigo da troca que deseja selecionar: ")
+        return codigo
+
+    def pega_dados_saida_veiculo(self):
+        codigo_oleo = input("CODIGO DE OLEO A SER UTILIZADO: ")
+        data_saida = input("DATA DE SAIDA DO VEICULO: ")
+        return {"codigo_oleo": codigo_oleo, "data_saida": data_saida}
 
     def pega_dados_troca(self):
         print("-------- DADOS TROCA DE OLEO ----------")
-        cpf_cliente = input("CPF do Cliente ")
         placa_moto = input("Placa do veículo: ")
+        data_entrada = input("Data de entrada do veiculo: ")
 
-        return {"cpf_cliente": cpf_cliente, "placa": placa_moto}
+        return {"placa_moto": placa_moto, "data_entrada": data_entrada}
 
     def pega_dados_cliente(self):
         print("-------- DADOS CLIENTE ----------")
