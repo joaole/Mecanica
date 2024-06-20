5a35f808c1f9189439d2282b4930a8bc7d51fd32
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-06-20 16:24:54 -0300

    Create fornecedor_dao.py

---
+++ Mecanica-main/persistencia/fornecedor_dao.py
@@ -0,0 +1,16 @@
+from persistencia.dao import DAO
+from entidade.fornecedor import Fornecedor
+
+
+class FornecedorDAO(DAO):
+
+    def __init__(self):
+        super().__init__('oleos.pkl')
+
+    def add(self, fornecedor: Fornecedor):
+        if isinstance(fornecedor, Fornecedor):
+            super().add(fornecedor.cnpj, fornecedor)
+
+    def remove(self, fornecedor: Fornecedor):
+        if isinstance(fornecedor, Fornecedor):
+            super().remove(fornecedor.cnpj)
