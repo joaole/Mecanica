17d7a9f5073f5f769742dbae0867d185d8d0d510
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-06-24 13:57:21 -0300

    Update tela_sistema.py

--- Mecanica-main/limite/tela_sistema.py
+++ Mecanica-main/limite/tela_sistema.py
@@ -20,6 +20,8 @@ class TelaSistema:
             opcao = 3
         if values['4']:
             opcao = 4
+        if values['5']:
+            opcao = 5
 
         # cobre os casos de voltar, não clicar em nada e fechar janela, ou clicar cancelar
         if values['0'] or button in (None,'Cancelar'):
@@ -38,8 +40,9 @@ class TelaSistema:
             [sg.Text('Escolha sua opção', font=("Helvica",15))],
             [sg.Radio('Fornecedor',"RD1", key='1')],
             [sg.Radio('Oleos',"RD1", key='2')],
-            [sg.Radio('Clientes',"RD1", key='3')],
-            [sg.Radio('Troca de OLeo', "RD1", key='4')],
+            [sg.Radio('Modelos',"RD1", key='3')],
+            [sg.Radio('Clientes', "RD1", key='4')],
+            [sg.Radio('Troca de Oleo', "RD1", key='5')],
             [sg.Radio('Finalizar sistema',"RD1", key='0')],
             [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
