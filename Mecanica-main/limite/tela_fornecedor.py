a930d575595e8de104628fbae00457c79349c36c
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-06-24 18:37:50 -0300

    Update tela_fornecedor.py

--- Mecanica-main/limite/tela_fornecedor.py
+++ Mecanica-main/limite/tela_fornecedor.py
@@ -11,8 +11,8 @@ class TelaFornecedor():
     button, values = self.open()
     if values['1']:
       opcao = 1
-    if values['2']:
-      opcao = 2
+    if values['3']:
+      opcao = 3
     # cobre os casos de Retornar, fechar janela, ou clicar cancelar
     #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
     if values['0'] or button in (None, 'Cancelar'):
@@ -24,14 +24,14 @@ class TelaFornecedor():
     #sg.theme_previewer()
     sg.ChangeLookAndFeel('DarkTeal4')
     layout = [
-      [sg.Text('-------- FORNECEDORES ----------', font=("Helvica", 25))],
+      [sg.Text('-------- Fornecedor ----------', font=("Helvica", 25))],
       [sg.Text('Escolha sua opção', font=("Helvica", 15))],
       [sg.Radio('Incluir Fornecedor', "RD1", key='1')],
-      [sg.Radio('Listar Fornecedores', "RD1", key='2')],
+      [sg.Radio('Listar Fornecedores', "RD1", key='3')],
       [sg.Radio('Retornar', "RD1", key='0')],
       [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
     ]
-    self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
+    self.__window = sg.Window('SisTrica de Oleo').Layout(layout)
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   # opção de tratamento: adicionar um if e só coletar nome e telefone se o button é 'Confirmar'
@@ -41,58 +41,57 @@ class TelaFornecedor():
       [sg.Text('-------- DADOS FORNECEDOR ----------', font=("Helvica", 25))],
       [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
       [sg.Text('Telefone:', size=(15, 1)), sg.InputText('', key='telefone')],
-      [sg.Text('CNPJ:', size=(15, 1)), sg.InputText('', key='cnpj')],
-      [sg.Text('EMAIL:', size=(15, 1)), sg.InputText('', key='email')],
+      [sg.Text('Email', size=(15, 1)), sg.InputText('', key='email')],
+      [sg.Text('Cnpj:', size=(15, 1)), sg.InputText('', key='cnpj')],
       [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
     ]
-    self.__window = sg.Window('FORNECEDOR').Layout(layout)
+    self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
 
     button, values = self.open()
     nome = values['nome']
     telefone = values['telefone']
-    cnpj = values['cnpj']
     email = values['email']
+    cnpj= values['cnpj']
 
     self.close()
-    return {"nome": nome, "telefone": telefone, "cnpj": cnpj, 'email': email}
+    return {"nome": nome, "telefone": telefone, "email": email, "cnpj": cnpj}
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def mostra_fornecedor(self, dados_fornecedor):
+    sg.ChangeLookAndFeel('DarkTeal4')
+    # Cabeçalhos da tabela
+    header = ['Nome', 'Telefone', 'Email', 'Cnpj']
     layout = [
-              [sg.Table(values= dados_fornecedor, headings=['nome', 'cnpj'], max_col_width=25, background_color='DarkTeal4',
-                        auto_size_columns=True,
-                        display_row_numbers=True,
-                        justification='right',
-                        num_rows=20,
-                        alternating_row_color='lightyellow',
-                        key='-TABLE-',
-                        tooltip='This is a table')],
-
-              [sg.Button('Incluir', key=1), sg.Button('Alterar', key=2)], sg.Button('Excluir', key=3), sg.Button('Voltar', key=0),
+      [sg.Text('-------- LISTA DE FORNECEDORES ----------', font=("Helvetica", 25))],
+      [sg.Table(values=dados_fornecedor,
+                headings=header,
+                display_row_numbers=True,
+                auto_size_columns=True,
+                num_rows=min(15, len(dados_fornecedor)),
+                key='-TABLE-',
+                row_height=25)],
+      [
+        sg.Button('Incluir Fornecedor', key=1), sg.Button('Alterar Fornecedor', key=2), sg.Button('Excluir Fornecedor', key=4)
+      ],
+      [sg.Button('Voltar')]
     ]
-
-    self.__window = sg.Window('Lista de Fornecedores', layout)
-
-    button, values = self.open()
-
-    self.close()
-    return button, values
+    self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-  def seleciona_fornecedor(self):
+  def seleciona_amigo(self):
     sg.ChangeLookAndFeel('DarkTeal4')
     layout = [
-      [sg.Text('-------- SELECIONAR FORNECEDOR ----------', font=("Helvica", 25))],
-      [sg.Text('Digite o CNPJ do fornecedor que deseja selecionar:', font=("Helvica", 15))],
-      [sg.Text('CNPJ:', size=(15, 1)), sg.InputText('', key='cnpj')],
+      [sg.Text('-------- SELECIONAR AMIGO ----------', font=("Helvica", 25))],
+      [sg.Text('Digite o CPF do amigo que deseja selecionar:', font=("Helvica", 15))],
+      [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
       [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
     ]
-    self.__window = sg.Window('Seleciona Fornecedor').Layout(layout)
+    self.__window = sg.Window('Seleciona amigo').Layout(layout)
 
     button, values = self.open()
-    cnpj = values['cnpj']
+    cpf = values['cpf']
     self.close()
-    return cnpj
+    return cpf
 
   def mostra_mensagem(self, msg):
     sg.popup("", msg)
