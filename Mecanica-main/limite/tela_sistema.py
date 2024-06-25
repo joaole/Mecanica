9cb78689cfc753d2776e015cc7df0925b354bd07
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-06-24 21:45:21 -0300

    Update tela_sistema.py

--- Mecanica-main/limite/tela_sistema.py
+++ Mecanica-main/limite/tela_sistema.py
@@ -48,10 +48,9 @@ class TelaSistema:
             [sg.Radio('Modelos',"RD1", key='3')],
             [sg.Radio('Clientes', "RD1", key='4')],
             [sg.Radio('Troca de Oleo', "RD1", key='5')],
-            [sg.Radio('Finalizar sistema',"RD1", key='0')],
-            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            [sg.Button('Confirmar'), sg.Cancel('Finalizar Sistema')]
         ]
-        self.__window = sg.Window('SisTroca de Oleo').Layout(layout)sperado
+        self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
 # precisa chamar self.init_components() aqui para o caso de chamar essa janela uma 2a vez. Não é possível reusar layouts de janelas depois de fechadas.
     def tela_opcoes(self):
         sg.ChangeLookAndFeel('DarkTeal4')
