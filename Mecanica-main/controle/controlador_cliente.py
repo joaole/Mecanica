d8525d80464ae64d3280091c62b80e1370294ac5
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-06-27 16:58:16 -0300

    Update controlador_cliente.py

--- Mecanica-main/controle/controlador_cliente.py
+++ Mecanica-main/controle/controlador_cliente.py
@@ -1,212 +1,103 @@
-from entidade.cliente import Cliente
-from entidade.veiculo import Veiculo
-from limite.tela_cliente import TelaCliente
-from persistencia.cliente_dao import CLienteDAO
-import re
+from limite.tela_modelo import TelaModelo
+from entidade.modelo import Modelo
+from persistencia.modelo_dao import ModeloDAO
 
-
-class ControladorCliente:
+class ControladorModelo:
     def __init__(self, controlador_sistema):
+        self.__modelo_dao = ModeloDAO()
+        self.__tela_modelo = TelaModelo()
         self.__controlador_sistema = controlador_sistema
-        self.__tela_cliente = TelaCliente()
-        self.__cliente_dao = CLienteDAO()
 
-    def listar_clientes(self):
-        for cliente in self.__cliente_dao.get_all():
-            self.__tela_cliente.mostra_cliente({
-                "nome": cliente.nome,
-                "telefone": cliente.telefone,
-                "cpf": cliente.cpf,
-                "email": cliente.email
-            })
+    def gerar_codigo(self):
+        codigo = str(self.__modelo_dao.get_size() + 1)
+        return codigo
 
-    def pega_cliente_por_cpf(self, cpf: int):
-        for cliente in self.__cliente_dao.get_all():
-            if cliente.cpf == cpf:
-                return cliente
+    def pega_modelo_por_codigo(self, codigo: int):
+        for modelo in self.__modelo_dao.get_all():
+            if modelo.codigo == codigo:
+                return modelo
+        return None
+
+    def incluir_modelo(self):
+        dados_modelo = self.__tela_modelo.pega_dados_modelo()
+
+        for m in self.__modelo_dao.get_all():
+            if m.nome == dados_modelo["nome"] and m.expessura == dados_modelo["expessura"] and m.quantidade_oleo == dados_modelo["Quantidade de oleo"]:
+                self.__tela_modelo.mostra_mensagem("ATENCAO: Modelo já existente")
+                break
         else:
+            codigo = self.gerar_codigo()
+            novo_modelo = Modelo(dados_modelo["nome"], dados_modelo["expessura"], dados_modelo["Quantidade de oleo"], codigo)
+            self.__modelo_dao.add(novo_modelo)
+            self.__tela_modelo.mostra_mensagem("Modelo cadastrado com sucesso")
+
+    def alterar_modelo(self):
+        self.lista_modelos()
+        modelo_codigo = self.__tela_modelo.seleciona_modelo()
+        modelo = self.pega_modelo_por_codigo(modelo_codigo)
+
+        if modelo is not None:
+            novos_dados_modelo = self.__tela_modelo.pega_dados_modelo()
+            modelo.nome = novos_dados_modelo["nome"]
+            modelo.quantidade_oleo = novos_dados_modelo["Quantidade de oleo"]
+            modelo.expessura = novos_dados_modelo["expessura"]
+            self.lista_modelos()
+        else:
+            self.__tela_modelo.mostra_mensagem("ATENCAO: Amigo não existente")
+
+    def lista_modelos(self):
+        if self.__modelo_dao.get_size() == 0:
             return None
-
-    def pega_moto_por_placa(self, cliente, placa_moto):
-        for veiculo in cliente.veiculos:
-            if veiculo.placa_moto == placa_moto:
-                return veiculo
         else:
-            return None
+            for modelo in self.__modelo_dao.get_all():
+                self.__tela_modelo.mostra_modelo({"nome": modelo.nome, "quantidade_oleo": modelo.quantidade_oleo, "expessura": modelo.expessura, "codigo": modelo.codigo})
+                return True
 
-    def inclui_cliente(self):
-        dados = self.__tela_cliente.pega_dados_cliente()
-        cpf = dados["cpf"]
-        telefone = dados["telefone"]
-        email = dados["email"]
+    def excluir_modelo(self):
+        self.lista_modelos()
+        codigo_modelo = self.__tela_modelo.seleciona_modelo()
+        modelo = self.pega_modelo_por_codigo(codigo_modelo)
 
-        if not self.verifica_cpf(cpf):
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: CPF inválido")
-            return
-        if not self.verifica_telefone(telefone):
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Telefone inválido")
-            return
-        if not self.verifica_email(email):
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Email inválido")
-            return
-
-        novo_cliente = Cliente(dados["nome"], telefone, email, cpf)
-        if self.pega_cliente_por_cpf(novo_cliente.cpf) is None:
-            self.__cliente_dao.add(novo_cliente)
-            self.__tela_cliente.mostra_mensagem(f"{novo_cliente.nome} cadastrado com sucesso.")
+        if modelo is not None:
+          self.__modelo_dao.remove(modelo)
+          self.lista_modelos()
+          self.__tela_modelo.mostra_mensagem("Modelo removido com sucesso.")
         else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: CPF duplicado")
-
-    def altera_cliente(self):
-        self.listar_clientes()
-        cpf_cliente = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf_cliente)
-        if cliente is not None:
-            novos_dados_cliente = self.__tela_cliente.pega_dados_cliente()
-            if not self.verifica_cpf(novos_dados_cliente["cpf"]):
-                self.__tela_cliente.mostra_mensagem("ATENÇÃO: CPF inválido")
-                return
-            if not self.verifica_telefone(novos_dados_cliente["telefone"]):
-                self.__tela_cliente.mostra_mensagem("ATENÇÃO: Telefone inválido")
-                return
-            if not self.verifica_email(novos_dados_cliente["email"]):
-                self.__tela_cliente.mostra_mensagem("ATENÇÃO: Email inválido")
-                return
-            cliente.nome = novos_dados_cliente["nome"]
-            cliente.cpf = novos_dados_cliente["cpf"]
-            cliente.email = novos_dados_cliente["email"]
-            cliente.telefone = novos_dados_cliente["telefone"]
-            self.__tela_cliente.mostra_mensagem("Cliente alterado com sucesso")
-        else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Cliente não existente")
-
-    def exclui_cliente(self):
-        self.listar_clientes()
-        cpf_cliente = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf_cliente)
-        if cliente is not None:
-            self.__cliente_dao.remove(cliente)
-        else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Cliente não existente")
-
-    def inclui_veiculo(self):
-        self.listar_clientes()
-        cpf = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf)
-        if cliente is not None:
-            self.__controlador_sistema.controlador_modelo.lista_modelos()
-            dados = self.__tela_cliente.pega_dados_moto()
-            modelo = self.__controlador_sistema.controlador_modelo.pega_modelo_por_codigo(dados["modelo"])
-            if modelo is not None:
-                nova_moto = Veiculo(dados["placa"], dados["kilometragem"], modelo)
-                if self.pega_moto_por_placa(cliente, nova_moto.placa_moto) is None:
-                    cliente.inclui_veiculo(nova_moto)
-                    self.__tela_cliente.mostra_mensagem("Veiculo cadastrado com sucesso")
-                else:
-                    self.__tela_cliente.mostra_mensagem("ATENÇÃO: Veículo já cadastrado.")
-            else:
-                self.__tela_cliente.mostra_mensagem("ATENCAO: Modelo nao existente")
-        else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Cliente não existe")
-
-    def exclui_veiculo(self):
-        self.listar_clientes()
-        cpf = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf)
-        if cliente is not None:
-            self.listar_veiculos_cliente(cliente)
-            placa_moto = self.__tela_cliente.seleciona_moto()
-            moto = self.pega_moto_por_placa(cliente, placa_moto)
-            if moto is not None:
-                cliente.veiculos.remove(moto)
-                self.__tela_cliente.mostra_mensagem("Veiculo removido com sucesso.")
-            else:
-                self.__tela_cliente.mostra_mensagem("ATENÇÃO: Moto não cadastrada")
-        else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Cliente não cadastrado")
-
-    def altera_veiculo(self):
-        self.listar_clientes()
-        cpf_cliente = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf_cliente)
-        if cliente is not None:
-            self.listar_veiculos_cliente(cliente)
-            placa = self.__tela_cliente.seleciona_moto()
-            novos_dados_moto = self.__tela_cliente.pega_dados_moto()
-            for veiculo in cliente.veiculos:
-                if veiculo.placa_moto == placa:
-                    veiculo.placa_moto = novos_dados_moto["placa"]
-                    veiculo.km_moto = novos_dados_moto["kilometragem"]
-                    self.__tela_cliente.mostra_mensagem("Veiculo alterado com sucesso")
-                    break
-            else:
-                self.__tela_cliente.mostra_mensagem("ATENÇÃO: Nenhum veículo cadastrado com esta placa.")
-        else:
-            self.__tela_cliente.mostra_mensagem("ATENÇÃO: Nenhum cliente cadastrado com este CPF")
-
-    def listar_veiculos(self):
-        self.listar_clientes()
-        cpf = self.__tela_cliente.seleciona_cliente()
-        cliente = self.pega_cliente_por_cpf(cpf)
-        if cliente is not None:
-            self.listar_veiculos_cliente(cliente)
-        if len(cliente.veiculos) == 0:
-            self.__tela_cliente.mostra_mensagem("ATENCAO: Nenhum veiculo cadastrado.")
-
-    def listar_veiculos_cliente(self, cliente):
-        for veiculo in cliente.veiculos:
-            self.__tela_cliente.mostra_veiculo({
-                    "modelo": veiculo.modelo.nome,
-                    "kilometragem": veiculo.km_moto,
-                    "placa": veiculo.placa_moto
-                })
-
-    def verifica_cpf(self, cpf):
-        cpf = ''.join(filter(str.isdigit, cpf))
-        if len(cpf) != 11:
-            return False
-
-        if cpf == cpf[0] * 11:
-            return False
-
-        def calcula_digito(cpf, pesos):
-            soma = sum(int(digito) * peso for digito, peso in zip(cpf, pesos))
-            resto = soma % 11
-            return 0 if resto < 2 else 11 - resto
-        pesos1 = range(10, 1, -1)
-        pesos2 = range(11, 1, -1)
-        digito1 = calcula_digito(cpf[:9], pesos1)
-        digito2 = calcula_digito(cpf[:10], pesos2)
-        return cpf[-2:] == f"{digito1}{digito2}"
-
-    def verifica_telefone(self, telefone):
-        telefone = ''.join(filter(str.isdigit, telefone))
-        return len(telefone) in [10, 11]
-
-    def verifica_email(self, email):
-        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        return re.match(pattern, email) is not None
-
-    def abre_tela(self):
-        lista_opcoes = {
-            1: self.inclui_cliente,
-            2: self.altera_cliente,
-            3: self.listar_clientes,
-            4: self.exclui_cliente,
-            5: self.inclui_veiculo,
-            6: self.altera_veiculo,
-            7: self.exclui_veiculo,
-            8: self.listar_veiculos,
-            0: self.retornar
-        }
-
-        continua = True
-        while continua:
-            opcao = self.__tela_cliente.tela_opcoes()
-            if opcao in lista_opcoes:
-                lista_opcoes[opcao]()
-            else:
-                self.__tela_cliente.mostra_mensagem("Opção inválida")
+          self.__tela_modelo.mostra_mensagem("ATENCAO: Modelo não existente")
 
     def retornar(self):
         self.__controlador_sistema.abre_tela()
+
+    def adicionar_oleo(self, oleo):
+        for modelo in self.__modelo_dao.get_all():
+            if modelo.expessura == oleo.expessura:
+                modelo.incluir_oleo(oleo)
+
+    def remover_oleo(self, oleo):
+        for md in self.__modelo_dao.get_all():
+            if md.expessura == oleo.expessura:
+                for ol in md.oleos:
+                    if ol.codigo == oleo.codigo:
+                        if md.exclui_oleo(oleo) is not None:
+                            return "Oleo Removido"
+                else:
+                    return "Oleo nao encontrado"
+        else:
+            return "Modelo nao encontrado"
+    def listar_oleos(self):
+        self.lista_modelos()
+        codigo = self.__tela_modelo.seleciona_modelo()
+        modelo = self.pega_modelo_por_codigo(codigo)
+        if modelo is not None:
+            self.listar_oleos_do_modelo(modelo)
+
+    def listar_oleos_do_modelo(self, modelo):
+        for oleo in modelo.oleos:
+            self.__tela_modelo.mostra_oleo_modelo({"fornecedor": oleo.fornecedor.cnpj, "marca": oleo.marca, "valor": oleo.valor, "codigo": oleo.codigo})
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.incluir_modelo, 2: self.alterar_modelo, 3: self.lista_modelos, 4: self.excluir_modelo, 5: self.listar_oleos, 0: self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_modelo.tela_opcoes()]()
