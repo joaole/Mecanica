47fc0e037d7767f3948f6c1d575c47da02d9fc0c
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-06-28 11:21:30 -0300

    Update tela_sistema.py

--- Mecanica-main/limite/tela_sistema.py
+++ Mecanica-main/limite/tela_sistema.py
@@ -5,53 +5,6 @@ class TelaSistema:
         self.__window = None
         self.init_components()
 
-# fazer aqui tratamento dos dados, caso a entrada seja diferente do eimport PySimpleGUI as sg
-
-class TelaSistema:
-    def __init__(self):
-        self.__window = None
-        self.init_components()
-
-# fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
-# precisa chamar self.init_components() aqui para o caso de chamar essa janela uma 2a vez. Não é possível reusar layouts de janelas depois de fechadas.
-    def tela_opcoes(self):
-        self.init_components()
-        button, values = self.__window.Read()
-        opcao = 0
-        if values['1']:
-            opcao = 1
-        if values['2']:
-            opcao = 2
-        if values['3']:
-            opcao = 3
-        if values['4']:
-            opcao = 4
-        if values['5']:
-            opcao = 5
-        # cobre os casos de voltar, não clicar em nada e fechar janela, ou clicar cancelar
-        if values['0'] or button in (None,'Cancelar'):
-            opcao = 0
-        self.close()
-        return opcao
-
-    def close(self):
-        self.__window.Close()
-
-    def init_components(self):
-        #sg.theme_previewer()
-        sg.ChangeLookAndFeel('DarkTeal4')
-        layout = [
-            [sg.Text('Bem vindo ao SisTroca de Oleo!', font=("Helvica",25))],
-            [sg.Text('Escolha sua opção', font=("Helvica",15))],
-            [sg.Radio('Fornecedores',"RD1", key='1')],
-            [sg.Radio('Oleos',"RD1", key='2')],
-            [sg.Radio('Modelos',"RD1", key='3')],
-            [sg.Radio('Clientes', "RD1", key='4')],
-            [sg.Radio('Troca de Oleo', "RD1", key='5')],
-            [sg.Button('Confirmar'), sg.Cancel('Finalizar Sistema')]
-        ]
-        self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
-# precisa chamar self.init_components() aqui para o caso de chamar essa janela uma 2a vez. Não é possível reusar layouts de janelas depois de fechadas.
     def tela_opcoes(self):
         sg.ChangeLookAndFeel('DarkTeal4')
         self.init_components()
@@ -69,7 +22,7 @@ class TelaSistema:
             opcao = 5
 
         # cobre os casos de voltar, não clicar em nada e fechar janela, ou clicar cancelar
-        if values['0'] or button in (None,'Cancelar'):
+        if button in (None, 'Cancelar', 'Finalizassss Sistema'):
             opcao = 0
         self.close()
         return opcao
@@ -78,17 +31,15 @@ class TelaSistema:
         self.__window.Close()
 
     def init_components(self):
-        #sg.theme_previewer()
         sg.ChangeLookAndFeel('DarkTeal4')
         layout = [
-            [sg.Text('Bem vindo ao SisTroca de Oleo!', font=("Helvica",25))],
-            [sg.Text('Escolha sua opção', font=("Helvica",15))],
-            [sg.Radio('Fornecedor',"RD1", key='1')],
-            [sg.Radio('Oleos',"RD1", key='2')],
-            [sg.Radio('Modelos',"RD1", key='3')],
+            [sg.Text('Bem vindo ao SisTroca de Oleo!', font=("Helvica", 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+            [sg.Radio('Fornecedor', "RD1", key='1')],
+            [sg.Radio('Oleos', "RD1", key='2')],
+            [sg.Radio('Modelos', "RD1", key='3')],
             [sg.Radio('Clientes', "RD1", key='4')],
             [sg.Radio('Troca de Oleo', "RD1", key='5')],
-            [sg.Radio('Finalizar sistema',"RD1", key='0')],
-            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            [sg.Button('Confirmar'), sg.Cancel('Finalizassss Sistema')]
         ]
         self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
