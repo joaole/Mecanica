8b508cf05621fb5e5350a3619c59a3270eda817c
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-06-29 22:28:32 -0300

    Update controlador_oleo.py

--- Mecanica-main/controle/controlador_oleo.py
+++ Mecanica-main/controle/controlador_oleo.py
@@ -27,7 +27,7 @@ class ControladorOleo:
         else:
             return None
 
-    def inclui_oleo(self):
+    def inclui_oleo(self, codigo=None):
         cnpj = self.__controlador_sistema.controlador_fornecedor.listar_fornecedores()
 
         fornecedor = self.__controlador_sistema.controlador_fornecedor.pega_fornecedor_por_cnpj(cnpj)
@@ -59,8 +59,9 @@ class ControladorOleo:
         else:
             self.__tela_oleo.mostra_mensagem("ATENÇÃO: Fornecedor não cadastrado")
 
-    def exclui_oleo(self):
-        codigo = self.__tela_oleo.seleciona_oleo()
+    def exclui_oleo(self, codigo=None):
+        if codigo is None:
+            codigo = self.__tela_oleo.seleciona_oleo()
         oleo = self.pega_oleo_por_codigo(codigo)
         if oleo is not None:
             self.__oleo_dao.remove(oleo)
@@ -68,12 +69,14 @@ class ControladorOleo:
         else:
             self.__tela_oleo.mostra_mensagem("ATENCAO: Oleo nao encontrado")
 
-    def altera_oleo(self):
-        self.lista_oleo()
-        codigo = self.__tela_oleo.seleciona_oleo()
+    def altera_oleo(self, codigo=None):
+        if codigo is None:
+            self.lista_oleo()
+            codigo = self.__tela_oleo.seleciona_oleo()
         oleo = self.pega_oleo_por_codigo(codigo)
-        dados = self.__tela_oleo.pega_dados_oleo()
+        dados = self.__tela_oleo.pega_dados_oleo({'marca': oleo.marca, 'expessura': oleo.expessura, 'valor': oleo.valor })
         if oleo is not None:
+
             oleo.marca = dados["marca"]
             oleo.valor = dados["valor"]
             oleo.expessura = dados["expessura"]
@@ -102,17 +105,19 @@ class ControladorOleo:
         lista_opcoes = {1: self.inclui_oleo, 2: self.altera_oleo, 3: self.lista_oleo_por_expessura,
                         4: self.exclui_oleo, 5: self.lista_oleo, 0: self.retornar}
 
-        lista_oleo= []
+        lista_oleo = []
         for oleo in self.__oleo_dao.get_all():
             lista_oleo.append([oleo.fornecedor.nome, oleo.marca, oleo.expessura, oleo.valor, oleo.codigo])
         dados_tela = self.__tela_oleo.tela_opcoes(lista_oleo)
         opcao = dados_tela['opcao']
         if opcao in lista_opcoes:
-            lista_opcoes[opcao](dados_tela['codigo'])
-        elif opcao == 0:
-            self.retornar()
+            if opcao == 0:
+                lista_opcoes[opcao]()
+            else:
+                lista_opcoes[opcao](dados_tela.get('codigo'))
         else:
-            self.__tela_oleo.mostra_mensagem('Opcção Invalida')
+            self.__tela_oleo.mostra_mensagem('Opção Inválida')
 
     def retornar(self):
         self.__controlador_sistema.abre_tela()
+
