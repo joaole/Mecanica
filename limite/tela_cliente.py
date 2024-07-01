ad8c4e53200f84eb053db0f47e5ed003684232c1
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-07-01 17:04:58 -0300

    Update tela_cliente.py

--- limite/tela_cliente.py
+++ limite/tela_cliente.py
@@ -9,18 +9,18 @@ class TelaCliente:
         pass
 
     def tela_opcoes(self, dados_cliente=[], dados_veiculo=[]):
-        sg.ChangeLookAndFeel('DarkTeal4')
+        sg.theme('DarkTeal4')
         # Cabeçalhos da tabela
-        header = ['Nome', 'Cpf', 'Telefone', 'Email']
-        cabecalho = ['Modelo', 'Placa', 'KM']
+        header_cliente = ['Nome', 'Cpf', 'Telefone', 'Email']
+        header_veiculo = ['Modelo', 'Placa', 'KM']
         layout = [
             [sg.Text('-------- LISTA DE CLIENTE ----------', font=("Helvetica", 25))],
             [sg.Table(values=dados_cliente,
-                      headings=header,
+                      headings=header_cliente,
                       display_row_numbers=True,
                       auto_size_columns=True,
                       num_rows=min(15, len(dados_cliente)),
-                      key='-TABLE-',
+                      key='-TABLE_CLIENTE-',
                       row_height=25)],
             [
                 sg.Button('Incluir Cliente', key=1), sg.Button('Alterar Cliente', key=2),
@@ -29,14 +29,17 @@ class TelaCliente:
 
             [sg.Text('-------- LISTA DE VEICULO ----------', font=("Helvetica", 25))],
             [sg.Table(values=dados_veiculo,
-                      headings=cabecalho,
+                      headings=header_veiculo,
                       display_row_numbers=True,
                       auto_size_columns=True,
                       num_rows=min(15, len(dados_veiculo)),
-                      key='-TABLE-',
+                      key='-TABLE_VEICULO-',
                       row_height=25)],
-            [sg.Button('Incluir Veiculo', key=5),
-             sg.Button('Alterar Veiculo', key=6), sg.Button('Excluir Veiculo', key=7)],  # This line was missing a comma
+            [
+                sg.Button('Incluir Veiculo', key=5),
+                sg.Button('Alterar Veiculo', key=6), sg.Button('Excluir Veiculo', key=7)
+            ],
+
             [sg.Button('Voltar', key=0)]
         ]
         self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
@@ -44,53 +47,80 @@ class TelaCliente:
         button, values = self.open()
         cpf = None
         placa = None
-        if values['-TABLE-']:
-            cpf = dados_cliente[values['-TABLE-'][0]][1]
-            placa = dados_cliente[values['-TABLE-'][0][5]]
-            self.close()
+        if values['-TABLE_CLIENTE-']:
+            cpf = dados_cliente[values['-TABLE_CLIENTE-'][0]][1]
+        if values['-TABLE_VEICULO-']:
+            placa = dados_veiculo[values['-TABLE_VEICULO-'][0]][1]
+        self.close()
 
         return {'opcao': button, 'cpf': cpf, 'placa': placa}
 
     def seleciona_cliente(self):
-        cpf = input("CPF do cliente que deseja selecionar: ")
+        layout = [
+            [sg.Text('CPF do cliente que deseja selecionar:')],
+            [sg.Input(key='-CPF-')],
+            [sg.Button('OK'), sg.Button('Cancelar')]
+        ]
+        self.__window = sg.Window('Selecionar Cliente', layout)
+        button, values = self.open()
+        cpf = values['-CPF-'] if button == 'OK' else None
+        self.close()
         return cpf
 
     def pega_codigo_modelo(self):
-        codigo_modelo = input("CODIGO DE MODELO DO VEICULO: ")
+        layout = [
+            [sg.Text('CODIGO DE MODELO DO VEICULO:')],
+            [sg.Input(key='-CODIGO_MODELO-')],
+            [sg.Button('OK'), sg.Button('Cancelar')]
+        ]
+        self.__window = sg.Window('Código do Modelo', layout)
+        button, values = self.open()
+        codigo_modelo = values['-CODIGO_MODELO-'] if button == 'OK' else None
+        self.close()
         return codigo_modelo
 
     def pega_dados_moto(self):
-        print("-------- DADOS MOTO ----------")
-        placa_moto = input("Placa: ")
-        km_moto = input("Kilometragem: ")
-
-        return {"placa": placa_moto, "kilometragem": km_moto}
+        layout = [
+            [sg.Text('-------- DADOS MOTO ----------')],
+            [sg.Text('Placa:'), sg.Input(key='-PLACA-')],
+            [sg.Text('Kilometragem:'), sg.Input(key='-KILOMETRAGEM-')],
+            [sg.Button('OK'), sg.Button('Cancelar')]
+        ]
+        self.__window = sg.Window('Dados da Moto', layout)
+        button, values = self.open()
+        dados_moto = {
+            "placa": values['-PLACA-'],
+            "kilometragem": values['-KILOMETRAGEM-']
+        } if button == 'OK' else None
+        self.close()
+        return dados_moto
 
     def pega_dados_cliente(self, dados_cliente={'nome': '', 'cpf': '', 'email': '', 'telefone': ''}):
-        sg.ChangeLookAndFeel('DarkTeal4')
+        sg.theme('DarkTeal4')
         layout = [
-            [sg.Text('-------- DADOS CLIENTE ----------', font=("Helvica", 25))],
+            [sg.Text('-------- DADOS CLIENTE ----------', font=("Helvetica", 25))],
             [sg.Text('Nome:', size=(15, 1)), sg.InputText(dados_cliente['nome'], key='nome')],
             [sg.Text('Telefone:', size=(15, 1)), sg.InputText(dados_cliente['telefone'], key='telefone')],
-            [sg.Text('Email', size=(15, 1)), sg.InputText(dados_cliente['email'], key='email')],
+            [sg.Text('Email:', size=(15, 1)), sg.InputText(dados_cliente['email'], key='email')],
             [sg.Text('Cpf:', size=(15, 1)), sg.InputText(dados_cliente['cpf'], key='cpf')],
-            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            [sg.Button('Confirmar'), sg.Button('Cancelar')]
         ]
         self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
-
         button, values = self.open()
-        nome = values['nome']
-        telefone = values['telefone']
-        email = values['email']
-        cpf = values['cpf']
-
+        if button == 'Confirmar':
+            dados_cliente = {
+                "nome": values['nome'],
+                "telefone": values['telefone'],
+                "email": values['email'],
+                "cpf": values['cpf']
+            }
         self.close()
-        return {"nome": nome, "telefone": telefone, "email": email, "cpf": cpf}
+        return dados_cliente
 
     def mostrar_cliente(self, dados_cliente):
-        sg.ChangeLookAndFeel('DarkTeal4')
-        # Cabeçalhos da tabela
-        header = ['Nome', 'Cpf', 'Telefone', 'Email']
+        sg.theme('DarkTeal4')
+        header = ['Nome', 'Cpf', 'Telefone', 'Email', 'Veiculo']
+        dados_cliente = self.formatar_dados_modelo(dados_cliente)
         layout = [
             [sg.Text('-------- LISTA DE CLIENTE ----------', font=("Helvetica", 25))],
             [sg.Table(values=dados_cliente,
@@ -100,41 +130,56 @@ class TelaCliente:
                       num_rows=min(15, len(dados_cliente)),
                       key='-TABLE-',
                       row_height=25)],
-            [
-                sg.Button('Incluir Cliente', key=1), sg.Button('Alterar Cliente', key=2),
-                sg.Button('Excluir Cliente', key=4), sg.Button('Incluir Veiculo', key=5),
-                sg.Button('Alterar Veiculo', key=6), sg.Button('Excluir Veiculo', key=7)
-            ],
             [sg.Button('Voltar', key=0)]
         ]
         self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
-
         button, values = self.open()
-        cpf = None
-        placa = None
-        if values['-TABLE-']:
-            cpf = dados_cliente[values['-TABLE-'][0]][1]
-            placa = dados_cliente[values['-TABLE-'][0][5]]
-            self.close()
-
-        return {'opcao': button, 'cpf': cpf, 'placa': placa}
+        self.close()
+        return button
 
     def mostra_veiculo(self, dados_veiculo):
-        print("MODELO DO VEICULO: ", dados_veiculo["modelo"])
-        print("KILOMETRAGEM: ", dados_veiculo["kilometragem"])
-        print("PLACA: ", dados_veiculo["placa"])
-        print("\n")
+        sg.theme('DarkTeal4')
+        header = ['Modelo', 'Placa', 'KM']
+        layout = [
+            [sg.Text('-------- LISTA DE VEICULO ----------', font=("Helvetica", 25))],
+            [sg.Table(values=dados_veiculo,
+                      headings=header,
+                      display_row_numbers=True,
+                      auto_size_columns=True,
+                      num_rows=min(15, len(dados_veiculo)),
+                      key='-TABLE-',
+                      row_height=25)],
+            [sg.Button('Voltar', key=0)]
+        ]
+        self.__window = sg.Window('SisTroca de Oleo').Layout(layout)
+        button, values = self.open()
+        self.close()
+        return button
 
     def seleciona_moto(self):
-        placa_moto = input("Placa da moto que deseja selecionar: ")
+        layout = [
+            [sg.Text('Placa da moto que deseja selecionar:')],
+            [sg.Input(key='-PLACA-')],
+            [sg.Button('OK'), sg.Button('Cancelar')]
+        ]
+        self.__window = sg.Window('Selecionar Moto', layout)
+        button, values = self.open()
+        placa_moto = values['-PLACA-'] if button == 'OK' else None
+        self.close()
         return placa_moto
+
+    def formatar_dados_modelo(self, dados_modelo):
+        # Garante que os dados_modelo sejam uma lista de listas de strings
+        if isinstance(dados_modelo, list) and all(isinstance(linha, list) for linha in dados_modelo):
+            return [[str(item) for item in linha] for linha in dados_modelo]
+        return []
 
     def mostra_mensagem(self, msg):
         sg.popup("", msg)
 
     def close(self):
-        self.__window.Close()
+        self.__window.close()
 
     def open(self):
-        button, values = self.__window.Read()
+        button, values = self.__window.read()
         return button, values
