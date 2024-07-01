6afa9ab19ad174b3c05b121e146b4e1b50c1a72e
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-07-01 16:01:17 -0300

    Update troca_de_oleo_entrada_dao.py

--- persistencia/troca_de_oleo_entrada_dao.py
+++ persistencia/troca_de_oleo_entrada_dao.py
@@ -1,22 +1,23 @@
 from persistencia.dao import DAO
 from entidade.troca_de_oleo import TrocaDeOleo
 
-
 class TrocaDeOleoEntradaDAO(DAO):
 
     def __init__(self):
         super().__init__('trocas_entrada.pkl')
+        TrocaDeOleo.carregar_proximo_codigo()  # Carregar o próximo código ao inicializar
 
     def load(self):
         super().load()
-        if self.get_all():
-            print(self.get_all()[0].get_proximo_codigo())
-            TrocaDeOleo.set_proximo_codigo(self.get_all()[0].get_proximo_codigo())
+        for troca_de_oleo in self.get_all():
+            TrocaDeOleo.set_proximo_codigo(troca_de_oleo.codigo + 1)
 
     def add(self, troca_de_oleo: TrocaDeOleo):
         if isinstance(troca_de_oleo, TrocaDeOleo):
             super().add(troca_de_oleo.codigo, troca_de_oleo)
+            TrocaDeOleo.salvar_proximo_codigo()
 
     def remove(self, troca_de_oleo: TrocaDeOleo):
         if isinstance(troca_de_oleo, TrocaDeOleo):
             super().remove(troca_de_oleo.codigo)
+            TrocaDeOleo.salvar_proximo_codigo()
