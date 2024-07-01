8c262680e4b371c2723f6adbe978bb4c9fb29957
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2024-07-01 16:00:11 -0300

    Update troca_de_oleo.py

--- entidade/troca_de_oleo.py
+++ entidade/troca_de_oleo.py
@@ -1,11 +1,14 @@
-from datetime import datetime
+from datetime import datetime, date
 from entidade.veiculo import Veiculo
 from entidade.cliente import Cliente
 from entidade.oleo import Oleo
-
+import pickle
 
 class TrocaDeOleo:
+
     __proximo_codigo = 0
+    __codigo_filename = 'proximo_codigo.pkl'  # Nome do arquivo pickle para o próximo código
+
     def __init__(self, veiculo, cliente, data_entrada):
         if isinstance(veiculo, Veiculo):
             self.__veiculo = veiculo
@@ -13,22 +16,26 @@ class TrocaDeOleo:
             self.__cliente = cliente
         self.__oleo = None
         self.__valor_final = None
-        self.__codigo = self.gerar_codigo()
+        self.__codigo = TrocaDeOleo.gerar_codigo()  # Chamando o método de classe para gerar código
         self.__data_entrada = data_entrada
         self.__data_saida = None
 
     @classmethod
-    def set_proximo_codigo(cls, proximo_codigo):
-        if proximo_codigo > TrocaDeOleo.__proximo_codigo:
-            TrocaDeOleo.__proximo_codigo = proximo_codigo
+    def gerar_codigo(cls):
+        cls.__proximo_codigo += 1
+        return cls.__proximo_codigo
 
     @classmethod
-    def get_proximo_codigo(cls):
-        return TrocaDeOleo.__proximo_codigo
+    def set_proximo_codigo(cls, proximo_codigo):
+        cls.__proximo_codigo = proximo_codigo
 
-    def gerar_codigo(self):
-        TrocaDeOleo.__proximo_codigo += 1
-        self.__codigo = TrocaDeOleo.__proximo_codigo
+    def __converter_para_date(self, data):
+        if isinstance(data, date):
+            return data
+        elif isinstance(data, datetime):
+            return data.date()
+        else:
+            raise ValueError("A data fornecida não é do tipo date ou datetime.")
 
     @property
     def veiculo(self):
@@ -75,15 +82,7 @@ class TrocaDeOleo:
 
     @data_entrada.setter
     def data_entrada(self, data_entrada):
-        self.__data_entrada = data_entrada
-
-    @valor_final.setter
-    def valor_final(self, valor_final):
-        self.__valor_final = valor_final
-
-    def calcula_valor_final(self, veiculo, oleo):
-        self.__valor_final = float(veiculo.modelo.quantidade_oleo) * float(oleo.valor)
-        return self.__valor_final
+        self.__data_entrada = self.__converter_para_date(data_entrada)
 
     @property
     def data_saida(self):
@@ -91,4 +90,21 @@ class TrocaDeOleo:
 
     @data_saida.setter
     def data_saida(self, data_saida):
-        self.__data_saida = data_saida
+        self.__data_saida = self.__converter_para_date(data_saida)
+
+    def calcula_valor_final(self, veiculo, oleo):
+        self.__valor_final = float(veiculo.modelo.quantidade_oleo) * float(oleo.valor)
+        return self.__valor_final
+
+    @classmethod
+    def salvar_proximo_codigo(cls):
+        with open(cls.__codigo_filename, 'wb') as f:
+            pickle.dump(cls.__proximo_codigo, f)
+
+    @classmethod
+    def carregar_proximo_codigo(cls):
+        try:
+            with open(cls.__codigo_filename, 'rb') as f:
+                cls.__proximo_codigo = pickle.load(f)
+        except FileNotFoundError:
+            cls.__proximo_codigo = 0
